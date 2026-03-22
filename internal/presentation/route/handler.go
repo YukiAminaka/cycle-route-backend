3e3927ce410c54c8dc276082b664a804d324446c
YukiAminaka <yukiaminaka28@gmail.com>
2026-03-22 21:35:26 +0900

    update: swaggerドキュメントを更新

--- internal/presentation/route/handler.go
+++ internal/presentation/route/handler.go
@@ -468,6 +468,20 @@ func (h *Handler) GetRoutesByUserID(c *gin.Context) {
 	response.ReturnStatusOK(c, res)
 }
 
+// ExportRouteGPX godoc
+//
+//	@Summary	ルートをGPX形式でエクスポートする
+//	@Tags		routes
+//	@Accept		json
+//	@Produce	application/gpx+xml
+//	@Security	CookieAuth
+//	@Param		route_id	path	string	true	"Route ID"
+//	@Success	200
+//	@Failure	400	{object}	response.ErrorResponse
+//	@Failure	401	{object}	response.ErrorResponse
+//	@Failure	404	{object}	response.ErrorResponse
+//	@Failure	500	{object}	response.ErrorResponse
+//	@Router		/routes/{route_id}/gpx [get]
 func (h *Handler) ExportRouteGPX(c *gin.Context) {
 	routeID := c.Param("route_id")
 	xmlBytes, err := h.exportGPXUsecase.ExportGPX(c.Request.Context(), routeID)
